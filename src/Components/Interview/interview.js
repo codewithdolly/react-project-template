1b382187922d67f350fd093fcd86d29249a01bf6
hicoderindia <hicoder.dolly@gmail.com>
2022-03-15 17:33:08 +0530

    Create interview.js

---
+++ src/Components/Interview/interview.js
@@ -0,0 +1,17 @@
+obj1 = { a: 10 };
+const obj2 = obj1;
+obj2.a = 20;
+console.log(obj1);
+console.log(obj2);
+let obj1; //obj one has not inishilized yet so it will show err "cannot inishilize before declaration."
+
+
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+} // it will print 3 times 3.
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}// it will print after previous loop. and it will print 0,1,2
+
