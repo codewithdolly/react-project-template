995211618b81e22caf167d8bd7b834925a70a66c
hicoderindia <hicoder.dolly@gmail.com>
2022-03-24 02:51:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import MainRouter from "./Components/Router/MainRouter"
+import React, {useState} from 'react'
+
+const obj=[
+  {
+    name:"Dolly",
+    roll:4321
+  },
+]
 
 const App = () => {
+  const [name, setName]= useState("radha");
+
+
+
+const updateObj= (e)=>{
+  const newValue= e.target.value
+  setName("I am Riti.")
+  setName(newValue)
+
+}
+
   return (
     <div>
-    
-    <MainRouter />
+    <h2>{name}</h2>
+    {/* <button onChange={updateObj}>click me</button> */}
+    <input type="text" placeholder= "type here" onChange={updateObj} />
     </div>
   )
 }
