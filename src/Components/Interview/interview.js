8141b9ad0b6f3ff7735a0d235ccf4c8e45e2820a
hicoderindia <hicoder.dolly@gmail.com>
2022-03-27 10:13:23 +0530

    Update interview.js

---
+++ src/Components/Interview/interview.js
@@ -0,0 +1,22 @@
+let arr = [2, 3, 5, 6, 8, 5, 3, 2, 4, 6, 7, 9, 6, 4, 2, 1];
+
+let sortNum= arr.sort();
+
+
+console.log(sortNum);
+
+
+
+let findNumCount=[];
+
+for (let i=0; i<sortNum.length; i++){
+    
+    if(sortNum[i]=== sortNum[i]++){
+        console.log( [i], findNumCount.push)
+    }
+    else{
+        console.log(sortNum[i]);
+    }
+   
+
+
